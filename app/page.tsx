b4df57577840c42f172711bf828242021e4d014d
AzawuntuBlatena <azabla1227@gmail.com>
2024-08-15 16:23:46 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -61,7 +61,7 @@ export default function Home() {
           <h2>
             Signup <span>-&gt;</span>
           </h2>
-          <p>Create acount and sell your book</p>
+          <p>Create acount and sell your books</p>
         </a>
 
         <a
